e7c9b04048f9e5d35928845c3565653e9db93598
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-05-20 17:52:35 -0300

    Adding initial code for hst::game:Game struct.

---
+++ src/Game.cpp
@@ -0,0 +1,46 @@
+#include "Game.hpp"
+#include "util.hpp"
+#include <SDL2/SDL.h>
+
+namespace hst {
+namespace game {
+
+Game::Game() 
+	: _win("Pong", WIDTH, HEIGHT, SDL_WINDOW_OPENGL), _ren(nullptr) {
+	_ren = SDL_CreateRenderer(_win.raw(), 0, 0);
+}
+
+void Game::run() {
+	bool running = true;
+	const int fps = 60;
+	SDL_Event evt;
+	while (running) {
+		auto starting_time = SDL_GetTicks();
+		while (SDL_PollEvent(&evt)) {
+			if (evt.type == SDL_KEYDOWN) {
+				running = evt.key.keysym.sym != SDLK_ESCAPE;
+			}
+			if (evt.type == SDL_QUIT) {
+				running = false;
+			}
+		}
+		update();
+		preRender();
+		render();
+		SDL_RenderPresent(_ren);
+		auto elapsed_time = SDL_GetTicks() - starting_time;
+		SDL_Delay(hst::util::clamp(1000U / fps - elapsed_time, 0U, 1000U / fps));
+	}
+}
+
+void Game::update() {
+}
+
+void Game::render() {
+}
+
+void Game::preRender() {
+}
+
+}
+}
---
+++ src/Game.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "Window.hpp"
+
+struct SDL_Renderer;
+
+namespace hst {
+namespace game {
+
+struct Game {
+	static const int FPS = 60;
+	static const int WIDTH = 640;
+	static const int HEIGHT = 480;
+	Game();
+	void update();
+	void render();
+	void preRender();
+	void run();
+private:
+	core::Window _win;
+	SDL_Renderer* _ren;
+};
+
+} //namespace game
+} //namespace hst
