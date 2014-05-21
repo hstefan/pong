3faab5b94ea4ec63fed85e8312fcc1cda2555481
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-05-20 23:02:21 -0300

    Adding shape utilities (point and rect for now).

---
+++ src/Shape.hpp
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <ostream>
+
+namespace hst {
+namespace shape {
+
+template <class T>
+struct Rect;
+template <class T>
+struct Point;
+
+template <class T>
+std::ostream& operator<<(std::ostream& stream, const Rect<T>& rect);
+template <class T>
+std::ostream& operator<<(std::ostream& stream, const Point<T>& point);
+
+template <class T>
+Rect<T> makeRect(const Point<T>& topLeft, const Point<T>& bottomRight);
+template <class T>
+Rect<T> makeRect(T x, T y, T w, T h);
+template <class T>
+Point<T> makePoint(T x, T y);
+template <class T>
+bool insideRect(const Point<T>& p, const Rect<T>& r);
+
+template <class T>
+struct Rect {
+	T x, y, w, h;
+};
+
+template <class T>
+struct Point {
+	T x, y;
+};
+
+template <class T>
+std::ostream& operator<<(std::ostream& stream, const Rect<T>& rect) {
+	stream << "rect(x:" << rect.x << ",y:" << rect.y << ",w:" << rect.w << ",h:" << rect.h << ")";
+	return stream;
+}
+template <class T>
+std::ostream& operator<<(std::ostream& stream, const Point<T>& point) {
+	stream << "point(" << point.x << "," << point.y << ")";
+	return stream;
+}
+
+template <class T>
+Rect<T> makeRect(const Point<T>& topLeft, const Point<T>& bottomRight) {
+	return makeRect(topLeft.x, topLeft.y, bottomRight.x - topLeft.x, bottomRight.y - topLeft.y);
+}
+
+template <class T>
+Rect<T> makeRect(T x, T y, T w, T h) {
+	return Rect<T> { x, y, w, h };
+}
+
+template <class T>
+Point<T> makePoint(T x, T y) {
+	return Point<T> { x, y };
+}
+
+template <class T>
+bool insideRect(const Point<T>& p, const Rect<T>& r) {
+	return (p.x >= r.x && p.y >= r.y) && (p.x <= (r.x + r.w) && p.y <= (r.y + r.h));
+}
+
+} //namespace core
+} //namespace hst
