42451179874d945795c56699058fe9e308ca0c59
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-05-22 01:10:11 -0300

    Workaround to allow us to compile stb libraries properly (filter doesn't seem to be working).

--- premake5.lua
+++ premake5.lua
@@ -1,34 +1,50 @@
 solution "pong"
 	configurations { "release", "debug" }
-
-	flags { "FatalWarnings" }
 	warnings "Extra"
-	targetdir "bin"
 
+	project "stb"
+		kind "StaticLib"
+		language "c"
+		files { "src/**.c", "src/**.h" }
+		filter "configurations:debug"
+			defines { "DEBUG" }
+			flags { "symbols" }
+			optimize "Off"
+		filter "configurations:release"
+			defines { "NDEBUG" }
+			optimize "On"
+		filter "system:windows" 
+			targetdir "bin/win32"
+			objdir "obj/win32"
+		filter "system:linux" 
+			targetdir "bin/lib64inux"
+			objdir "obj/linux"
+	
 	project "pong"
 		kind "ConsoleApp"
 		language "c++"
-		files { "src/**.cpp", "src/**.hpp", "src/**.c", "src/**.cpp" }
+		flags { "FatalWarnings" }
+		links { "SDL2", "SDL2_mixer", "stb" }
+
+		files { "src/**.cpp", "src/**.hpp" }
 		includedirs { "src" }
 
-		configuration "debug"
+		filter "configurations:debug"
 			defines { "DEBUG" }
 			flags { "symbols" }
 			optimize "Off"
 
-		configuration "release"
+		filter "configurations:release"
 			defines { "NDEBUG" }
 			optimize "On"
 
-		configuration "windows"
+		filter "system:windows"
 			links { "mingw32", "SDL2main" }
 			targetdir "bin/win32"
 			objdir "obj/win32"
 
-		configuration "linux"
+		filter "system:linux"
 			targetdir "bin/linux"
 			objdir "obj/linux"
+			buildoptions { "-std=c++11" }
 
-		configuration "gmake"
-			buildoptions { "-std=c++11", "-Wall"}
-			links { "SDL2", "SDL2_mixer" }
