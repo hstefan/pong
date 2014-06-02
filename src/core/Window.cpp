7521fa91704a387bb20ca19d66a74476134347e1
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-06-01 21:08:41 -0300

    Adding GLWindow class.

--- src/core/Window.cpp
+++ src/core/Window.cpp
@@ -1,15 +1,17 @@
+#include "gl_core_3_3.hpp"
 #include "Window.hpp"
+#include <iostream>
 
 namespace hst {
 namespace core {
 
-Window::Window(const std::string& title, int w, int h, Uint32 flags) 
+Window::Window(const std::string& title, int w, int h, Uint32 flags)
 	: Window(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, w, h, flags) {
 }
 
-Window::Window(const std::string& title, int x, int y, int w, int h, Uint32 flags) 
+Window::Window(const std::string& title, int x, int y, int w, int h, Uint32 flags)
 	: width(w), height(h), _win(nullptr) {
-	_win = SDL_CreateWindow(title.c_str(), x, y, w, h, flags);
+	_win = SDL_CreateWindow(title.c_str(), x, y, w, h, flags);	
 }
 
 Window::~Window() {
@@ -18,6 +20,24 @@ Window::~Window() {
 
 SDL_Window* Window::raw() {
 	return _win;
+}
+
+GLWindow::GLWindow(const std::string& title, int x, int y, int w, int h, Uint32 flags) 
+	: Window(title, x, y, w, h, flags | SDL_WINDOW_OPENGL) {
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
+	SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+	_glContext = SDL_GL_CreateContext(_win);
+	auto didLoad = gl::sys::LoadFunctions();
+	if (didLoad) {
+		std::cout << "OpenGL Context Loaded: OpenGL " << gl::sys::GetMajorVersion() << "." 
+			<< gl::sys::GetMinorVersion() << std::endl;
+	} else {
+		std::cout << "OpenGL: " << didLoad.GetNumMissing() << std::endl;
+	}
+}
+
+void GLWindow::swapBuffers() {
+	SDL_GL_SwapWindow(_win);
 }
 
 } //namespace core
--- src/core/Window.hpp
+++ src/core/Window.hpp
@@ -10,15 +10,22 @@ namespace core {
 struct Window {
 	Window(const std::string& title, int width, int height, Uint32 flags);
 	Window(const std::string& title, int x, int y, int w, int h, Uint32 flags);
-	~Window();
+	virtual ~Window();
 
 	const int width;
 	const int height;
 	SDL_Window* raw();
 	bool poolEvent(SDL_Event& evt);
-private:
+	void swapBuffers();
+protected:
 	SDL_Window* _win;
 };
 
+struct GLWindow : public Window {
+	GLWindow(const std::string& title, int x, int y, int w, int h, Uint32 flags);
+	void swapBuffers();
+private:
+	SDL_GLContext _glContext;
+};
 } //namespace core 
 } //namespace hst
