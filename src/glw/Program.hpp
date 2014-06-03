5ae5415388a77033b79d7d9ad955e8af707dffb2
Hugo Puhlmann <hugopuhlmann@gmail.com>
2014-06-02 21:17:28 -0300

    "Porting" glw::Program to use glw::Object as its base class.

--- src/glw/Program.hpp
+++ src/glw/Program.hpp
@@ -3,21 +3,20 @@
 #include <string>
 #include <set>
 #include "gl_core_3_3.hpp"
+#include "Object.hpp"
 
 namespace hst {
 namespace glw {
 
-struct Program {
+struct Program : public Object {
 	Program();
-	~Program();
-	
+	~Program();	
 	bool attach_shader(GLuint s);
 	void detach_shader(GLuint s);
 	bool link();
 	std::string info_log();
 private:
 	std::set<GLuint> _shaders;
-	GLuint _id;
 };
 
 } //namespace glw
